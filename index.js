0eff4a043fad7d7d8a4b26316176db2c06d5cc9b
alpap <al.pap@live.com>
2021-04-10 00:52:27 +0200

    removed unused

---
+++ index.js
@@ -0,0 +1,63 @@
+import xlsx2json from 'xlsx2json'
+
+import QRCode from 'qrcode'
+
+async function GetDataFromExcel() {
+  const json_data = await xlsx2json('./123.xlsx', {
+    keysRow: 1,
+  })
+  const map = new Map()
+  json_data[0].forEach((element) => {
+    map.set(element['Assembly Mark'].trim(), element)
+  })
+  const list_from_map = []
+  map.forEach((value) => {
+    list_from_map.push(value)
+  })
+  return list_from_map
+}
+
+function PopulateSides(mark, side_letters, to) {
+  const list_of_objs = new Array()
+  side_letters.forEach((sideLetter) => {
+    for (let i = 0; i <= to; i++) {
+      const numToString = i.toString()
+      const side = `${sideLetter}${numToString.length === 1 ? '0' + numToString : numToString}`
+      list_of_objs.push({
+        qr_text: `${mark},${side}`,
+        qr: '',
+        side: side,
+        mark: mark,
+      })
+    }
+  })
+  return list_of_objs
+}
+
+async function CreateQrCodes(values) {
+  for (const value of values) {
+    value.qr = await QRCode.toDataURL(value.qr_text)
+  }
+}
+
+async function GenerateSides(values, side_c, to_number) {
+  if (!to_number) to_number = 97
+  const side_letters = ['A', 'B']
+  if (side_c) side_letters.push('C')
+
+  const map_of_assemblies = new Map()
+  for (const value of values) {
+    const mark = value['Assembly Mark'].trim()
+    const populated_side_data = PopulateSides(mark, side_letters, to_number)
+    await CreateQrCodes(populated_side_data)
+    map_of_assemblies.set(mark, populated_side_data)
+  }
+  return map_of_assemblies
+}
+
+async function main() {
+  const data = await GetDataFromExcel()
+  return await GenerateSides(data, true)
+}
+
+main().then((m) => m)
--- qr.js
+++
@@ -1,9 +0,0 @@
-import QRCode from 'qrcode'
-
-const generateQR = async (text) => {
-  try {
-    return await QRCode.toDataURL(text)
-  } catch (err) {
-    console.error(err)
-  }
-}
